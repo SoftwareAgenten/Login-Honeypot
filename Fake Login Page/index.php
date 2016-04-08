85fdcea96a320f3f97e9f91f188988f31a480cd2
Florian Pircher <git@leuchtschirm.com>
2016-04-08 21:43:51 +0200

    Implement and Style Form, Move Styles File

---
+++ Fake Login Page/index.php
@@ -0,0 +1,27 @@
+<?php
+
+include_once('great-attractor/system.php');
+
+?><!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>Login</title>
+    <link rel="stylesheet" href="data/assets/styles.css">
+  </head>
+  <body>
+    <h1>Login</h1>
+    
+    <form action="post">
+      <label for="email">Email Address</label>
+      <input type="email" name="email" id="email">
+      
+      <label for="password">Password</label>
+      <input type="password" name="password" value="" id="password">
+      
+      <input type="submit" />
+    </form>
+    
+    <p><a href="register.php">Register</a></p>
+  </body>
+</html>
